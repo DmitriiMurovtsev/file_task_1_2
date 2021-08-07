ec04fffcc62bec8c61f44c4661c448a896fbf8a5
Dmitrii Murovtsev <mdv12672@gmail.com>
2021-08-07 23:19:30 +0300

    2

--- job_files.py
+++ job_files.py
@@ -1,18 +1,23 @@
-with open('recipes.txt', 'r', encoding='utf-8') as recipes:
-    cook_book = {}
-    while True:
-        b = recipes.readline().strip()
-        my_list_1 = []
-        for i in range(0, int(recipes.readline())):
-            my_dict = {}
-            my_list = recipes.readline().strip().split(' | ')
-            my_dict['ingredient_name'] = my_list[0]
-            my_dict['quantity'] = int(my_list[1])
-            my_dict['measure'] = my_list[2]
-            my_list_1.append(my_dict)
-        cook_book[b] = my_list_1
-        if not recipes.readline():
-            break
+from pprint import pprint
+
+
+def create_dict_from_file(file_name):
+    cook_dict = {}
+    with open(file_name, 'r', encoding='utf-8') as recipes:
+        for line in recipes:
+            dish_name = line.strip()
+            list_of_ingridient = []
+            for i in range(int(recipes.readline())):
+                temp_dict = {}
+                my_list = recipes.readline().strip().split(' | ')
+                temp_dict['ingredient_name'] = my_list[0]
+                temp_dict['quantity'] = int(my_list[1])
+                temp_dict['measure'] = my_list[2]
+                list_of_ingridient.append(temp_dict)
+            cook_dict[dish_name] = list_of_ingridient
+            recipes.readline()
+    return cook_dict
+
 
 def food_preparation(list_food, person_count):
     my_dict = {}
@@ -26,3 +31,7 @@ def food_preparation(list_food, person_count):
                 my_dict_1['measure'] = j['measure']
                 my_dict[j['ingredient_name']] = my_dict_1
     return my_dict
+
+cook_book = create_dict_from_file('recipes.txt')
+pprint(food_preparation(['Запеченный картофель', 'Омлет'], 2))
+
